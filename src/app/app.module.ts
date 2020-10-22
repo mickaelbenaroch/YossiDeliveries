062dacf7eeb92c0c0efc3de483c5409093644b6a
mbenaroc <mickael_benaroch@bmc.com>
2020-10-22 16:52:32 +0300

    dsf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
